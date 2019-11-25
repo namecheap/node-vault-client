7527ab1e436cd9102230ce4dea9ca4e48a2b6e57
Vladimir Zholudev <vszholudev@yandex.ru>
2019-11-25 15:29:47 +0200

    Update jwt read (#8)
    * Update jwt read
    
    * debug
    
    * Update VaultKubernetesAuth.js
    
    * Add debug
    
    * Refactor and andd jwt file path
    
    * 0.5.5
    
    * Update package-lock.json

--- src/auth/VaultKubernetesAuth.js
+++ src/auth/VaultKubernetesAuth.js
@@ -5,38 +5,43 @@ class VaultKubernetesAuth extends VaultBaseAuth {
     /**
      * @param {VaultApiClient} apiClient - see {@link VaultBaseAuth#constructor}
      * @param {Object} config
-     * @param {String} config.role - Role of the Kubernetes.
-     * @param {String} [config.jwt] - Your service jwt token.
+     * @param {String} config.role - Role configured in Vault Kubernetes Auth backend under which we want to issue Vault token.
+     * @param {String} [config.tokenPath] - Path to the Kube JWT token. If omitted - default will be used.
      * @param {String} mount - Vault's  mount point ("kubernetes" by default)
      */
     constructor(apiClient, logger, config, mount) {
         super(apiClient, logger, mount || 'kubernetes');
 
         this.__role = config.role;
-        this.__filePath = '/var/run/secrets/kubernetes.io/serviceaccount/token';
+        this.__tokenPath = '/var/run/secrets/kubernetes.io/serviceaccount/token';
+        if (config.tokenPath !== undefined) {
+            this.__tokenPath = config.tokenPath;
+        }
     }
 
     _authenticate() {
         this._log.info(
-            'making authentication request: role_id=%s',
-            this.__roleId,
+            'making authentication request: Vault role: "%s"; K8s token path: "%s"',
+            this.__role, this.__tokenPath
         );
 
-        return Promise.resolve(fs.readFileSync(this.__filePath))
-            .then((jwt) => {
-                console.log(`File is contains token ${jwt}`)
-                return this.__apiClient.makeRequest('POST', `/auth/${this._mount}/login`, {
-                    role: this.__role,
-                    jwt,
-                });
-            })
-            .then((res) => {
-                this._log.debug(
-                    'receive token: %s',
-                    res.auth.client_token,
-                );
-                return this._getTokenEntity(res.auth.client_token);
-            });
+        const k8sJwtToken = fs.readFileSync(this.__tokenPath).toString();
+        this._log.debug(
+            'receive K8s token: %s',
+            k8sJwtToken
+        );
+
+        return this.__apiClient.makeRequest('POST', `/auth/${this._mount}/login`, {
+            role: this.__role,
+            jwt: k8sJwtToken,
+        }).then((res) => {
+            this._log.debug(
+                'receive token: %s',
+                res.auth.client_token,
+            );
+
+            return this._getTokenEntity(res.auth.client_token);
+        });
     }
 }
 
